21362482b8b9400f09845d72f09d2ae51f34f82e
IzaiasCF <izaiascorreafilho@gmail.com>
2025-09-17 14:24:30 -0300

    add botão

--- MauiAppBasic/MainPage.xaml.cs
+++ MauiAppBasic/MainPage.xaml.cs
@@ -5,8 +5,19 @@
         public MainPage()
         {
             InitializeComponent();
-        }    
-        
+        }
 
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                //Criando um simples botão
+                await DisplayAlert("Aviso!", "Olá, mundo!", "Ok");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Aviso!", ex.Message, "Ok");
+            }
+        }
     }
 }
