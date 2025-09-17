630d7eaab159c944f4ff057b239fd296091c9ec2
IzaiasCF <izaiascorreafilho@gmail.com>
2025-09-17 12:06:19 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppBasic/MauiAppBasic/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace MauiAppBasic
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+             
+    }
+
+}
