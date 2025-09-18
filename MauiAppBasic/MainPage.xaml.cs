d4523d98b661de392549917effa9524ecdf86653
IzaiasCF <izaiascorreafilho@gmail.com>
2025-09-18 09:39:59 -0300

    add adaptações de cores

--- MauiAppBasic/MainPage.xaml.cs
+++ MauiAppBasic/MainPage.xaml.cs
@@ -8,6 +8,12 @@
         public MainPage()
         {
             InitializeComponent();
+
+            string endereco = @"<iframe width=""560"" height=""315"" src=""https://www.youtube.com/embed/DeQ9CgfPgtI?si=Dp7yvY4Dsgoeu-zm"" title=""YouTube video player"" frameborder=""0"" allow=""accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"" referrerpolicy=""strict-origin-when-cross-origin"" allowfullscreen></iframe>";
+            ICFvideos.Source = new HtmlWebViewSource
+                {
+                    Html = endereco
+                };
         }
 
         private async void Button_Clicked(object sender, EventArgs e)
