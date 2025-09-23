c3085499fb4938343ce6f99efd8a9b17dd0c3255
IzaiasCF <izaiascorreafilho@gmail.com>
2025-09-23 14:02:08 -0300

    Add config

--- MauiAppBasic/MauiAppBasic/App.xaml.cs
+++ MauiAppBasic/MauiAppBasic/App.xaml.cs
@@ -6,7 +6,18 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new MainPage();
         }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            window.Width = 400;
+            window.Height = 650;
+
+            return window;
+        }
+
     }
 }
--- MauiAppBasic/MauiAppBasic/MainPage.xaml.cs
+++ MauiAppBasic/MauiAppBasic/MainPage.xaml.cs
@@ -1,14 +1,33 @@
 ﻿namespace MauiAppBasic
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : TabbedPage
     {
-        int count = 0;
+        //Inserindo nome
+        string nome = "";
 
         public MainPage()
         {
             InitializeComponent();
-        }
-             
-    }
 
+            string endereco = @"<iframe width=""560"" height=""315"" src=""https://www.youtube.com/embed/DeQ9CgfPgtI?si=Dp7yvY4Dsgoeu-zm"" title=""YouTube video player"" frameborder=""0"" allow=""accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"" referrerpolicy=""strict-origin-when-cross-origin"" allowfullscreen></iframe>";
+            ICFvideos.Source = new HtmlWebViewSource
+                {
+                    Html = endereco
+                };
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                nome = await DisplayPromptAsync("Olá!" , "Digite seu nome: ");
+                //Criando um simples botão
+                await DisplayAlert("", "Olá, " + nome + "!" , "Ok");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Aviso!", ex.Message, "Ok");
+            }
+        }
+    }
 }
